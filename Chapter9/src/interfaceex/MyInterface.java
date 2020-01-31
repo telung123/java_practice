5d69c06bd54bad34b81f55ba753ea96e0197fdff
GabinKim <mabin0910@gmail.com>
2020-01-31 18:01:56 +0900

    Interface 간 상속 예제

---
+++ Chapter9/src/interfaceex/MyInterface.java
@@ -0,0 +1,8 @@
+package interfaceex;
+
+public interface MyInterface extends X,Y {
+	// Interface 를 상속받는 인터페이스.
+	
+	void hello();
+
+}
---
+++ Chapter9/src/interfaceex/MyInterfaceClass.java
@@ -0,0 +1,24 @@
+package interfaceex;
+
+public class MyInterfaceClass implements MyInterface {
+
+	// 아래처럼 상속받은 모든 interface 의 메소드를 구현해야 함. 
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void yRun() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hello() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
