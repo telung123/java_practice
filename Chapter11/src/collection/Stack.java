5747229e3c91b95d670131f56d1e9b8d104e17c2
telung123 <mabin0910@gmail.com>
2020-02-03 21:25:01 +0900

    Stack 복습

--- Chapter11/src/collection/Stack.java
+++ Chapter11/src/collection/Stack.java
@@ -4,37 +4,35 @@ import java.util.ArrayList;
 
 class MyStack {
 	// LIFO Stack 구현해보기
-	
 	private ArrayList<String> arrayStack = new ArrayList<String>();
 	
 	public void push(String data) {
-		arrayStack.add(data);		
+		arrayStack.add(data);
 	}
 	
-	public String pop() {		
+	public String pop() {
 		int len = arrayStack.size();
-		if (len == 0) {
-			System.out.println("Stack 이 비어있습니다.");
+		if ( len == 0 ) {
+			System.out.println("Stack 이 비었습니다.");
 			return null;
-		}
-		
-		return arrayStack.remove(len - 1);
-	}	
-	
+		} 
+		return arrayStack.remove(len-1);
+	}
 }
 
 public class Stack {
 	
-	public static void main(String[] arg) {
+	public static void main(String[] args) {
 		MyStack stack = new MyStack();
-		stack.push("a");
-		stack.push("b");
-		stack.push("c");
+		stack.push("A");
+		stack.push("B");
+		stack.push("C");
 		
-		
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());		
+		System.out.println("POP : "+ stack.pop());
+		System.out.println("POP : "+ stack.pop());
+		System.out.println("POP : "+ stack.pop());
+		System.out.println("POP : "+ stack.pop());
+				
 	}
+
 }
